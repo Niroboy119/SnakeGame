9192dc736024797f4cd22a1499ced14a3c5f5115
saads2018 <71264405+saads2018@users.noreply.github.com>
2020-11-19 10:41:01 +0800

    Add files via upload

---
+++ SnakeGameTest/SnakeTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnakeGameTest;
+namespace SnakeGameTest
+{
+    [TestClass]
+    public class SnakeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
